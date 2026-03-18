0111635c71c584ef547d326ec5f75716338f1fe3
mxweng <80088258+mxweng@users.noreply.github.com>
2026-03-18 15:37:13 +0000

    Deploying to gh-pages from @ mxweng/mxweng.github.io@e61507fdcaa25ac15d383950f3bfc12c77f0d19a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,8 +16,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-curriculum-vitae",
-          title: "Curriculum Vitae",
+        },{id: "nav-cv",
+          title: "CV",
           description: "",
           section: "Navigation",
           handler: () => {
